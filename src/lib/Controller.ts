88d4838afd6249ae32fb25e9218a0a409dd97fff
Jan Lipnican <jan.lipnican@cognito.cz>
2017-03-07 19:20:55 +0100

    gitignore fix

---
+++ src/lib/Controller.ts
@@ -0,0 +1,91 @@
+import { Server } from './Server';
+
+export class Controller {
+
+    public route: string;
+
+    /**
+     * Runs while initializing controller
+     */
+    public onInit(): void { }
+
+    /**
+     * Runs after server start listen
+     */
+    public onStart(): void { }
+
+    /**
+     * Function attach hbs helper functions to response.
+     */
+    protected response(data: any): any{
+
+        let response = data;
+
+        if(Server.config().handlebars) {
+
+          this.configureHBS(response);
+
+        }
+
+        return response;
+
+    }
+
+    private configureHBS(response: any) {
+
+        response.helpers = this.constructor.prototype._hbs_helpers;
+
+        for (let key in response.helpers) {
+            response.helpers[key] = response.helpers[key].bind(this);
+        }
+
+    }
+
+}
+
+
+export enum RequestType {
+    POST = 1,
+    GET = 2,
+}
+
+export function Post(path: string) {
+
+    return baseRequestDecorator(path, RequestType.POST);
+
+}
+
+export function Get(path: string) {
+
+    return baseRequestDecorator(path, RequestType.GET);
+
+}
+
+export function HBS_helper(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
+
+    if(!Server.config().handlebars){
+
+        console.warn('For hbs helpers to work, "handlebars" property must be set in txconfig.');
+
+        return descriptor;
+
+    }
+
+    target._hbs_helpers = target._hbs_helpers || {};
+    target._hbs_helpers[propertyKey] = descriptor.value;
+
+    return descriptor;
+
+}
+
+export function baseRequestDecorator(path: string, type: RequestType) {
+
+    return (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) => {
+
+        target._routes = target._routes || [];
+        target._routes.push({path, method: descriptor.value, type});
+        return descriptor;
+
+    }
+
+}
---
+++ src/lib/Mongo.ts
@@ -0,0 +1,51 @@
+import * as mongoose from 'mongoose';
+
+// compiled models
+const models = {};
+
+export function model<T>(name: string, options?:any) {
+    const model = mongoose.model<mongoose.Document>(name, new mongoose.Schema(options));
+
+    return model;
+}
+
+export class Model<T> {
+
+    data: T;
+    name: string;
+    options: any;
+
+    constructor(data?: T) {
+
+        this.data = data;
+
+    }
+
+    public async save(): Promise<any> {
+
+        let dat = new this.model(this.data);
+
+        return await dat.save();
+
+    }
+
+    public async find(opt?: any): Promise<any> {
+
+        return await this.model.find(opt);
+
+    }
+
+    get model() {
+
+        if(typeof models[this.name] === 'undefined') {
+
+            models[this.name] = model<T>(this.name, this.options);
+
+        }
+
+        return models[this.name];
+
+    }
+
+}
+
